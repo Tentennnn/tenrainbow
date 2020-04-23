24ee0da6ac1cf51bb10a54e82179215c1df4d7cb
longlong5330 <60765259+longlong5330@users.noreply.github.com>
2020-04-23 15:28:34 +0700

    Create pingpong.js

---
+++ embed/pingpong.js
@@ -0,0 +1,8 @@
+{
+  "desc_1": "Is this really my ping!",
+  "desc_2": "Is it okey? I can look",
+  "desc_3": "I hope it isnt bad",
+  "bot_1": "🏓...Bot latency:",
+  "bot_2":  "🏓...API latency:"
+    
+}
